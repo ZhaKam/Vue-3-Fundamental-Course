80cdad52f33476f2a31d13fc3814cd1084928054
Zhakupova <puffy.eyes@mail.ru>
2022-04-04 11:28:23 +0600

    Deleting a post. KEY keys in the loop

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
     <post-form @create="createPost" />
-    <post-list :posts="posts" />
+    <post-list :posts="posts" @remove="removePost" />
   </div>
 </template>
 
@@ -25,7 +25,10 @@ export default {
   },
   methods: {
     createPost(post) {
-      this.posts.push(post)
+      this.posts.push(post);
+    },
+    removePost(post) {
+      this.posts = this.posts.filter((p) => p.id !== post.id);
     },
   },
 };
