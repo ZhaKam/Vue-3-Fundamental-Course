eef13c3ca2363154afbfe2efd695da21800aedb8
Zhakupova <puffy.eyes@mail.ru>
2022-04-04 09:16:04 +0600

    Post Item

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="post">
-    <div><strong>Название:</strong>{{ post.title }}</div>
-    <div><strong>Описание:</strong>{{ post.body }}</div>
+    <div><strong>Название: </strong>{{ post.title }}</div>
+    <div><strong>Описание: </strong>{{ post.body }}</div>
   </div>
 </template>
-
 <script>
-export default {};
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+};
 </script>
-
-<style scoped></style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="post" v-for="post in posts">
-    <div><strong>Название:</strong>{{ post.title }}</div>
-    <div><strong>Описание:</strong>{{ post.body }}</div>
+  <div>
+    <h3>Список пользователей:</h3>
+    <post-item v-for="post in posts" :post="post" />
   </div>
 </template>
 
-
 <script>
+import PostItem from "@/components/PostItem.vue";
 export default {
+  components: { PostItem },
   props: {
     posts: {
       type: Array,
@@ -18,9 +19,9 @@ export default {
 </script>
 
 <style scoped>
-.post {
+.post{
   padding: 15px;
   border: 2px solid teal;
-  margin: 15px;
+  margin-top: 15px;
 }
 </style>
