f62bcef0d45831d3abdeeba50d9024d6b6c1fe5d
Zhakupova <puffy.eyes@mail.ru>
2022-04-04 13:51:54 +0600

    Modal window

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
-    <post-form @create="createPost" />
+    <h1>Страница с постами</h1>
+    <my-button @click="showDialog">Создать пост</my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost" />
+    </my-dialog>
     <post-list :posts="posts" @remove="removePost" />
   </div>
 </template>
@@ -21,14 +25,19 @@ export default {
         { id: 3, title: "JavaScript 3", body: "Описание поста" },
         { id: 4, title: "JavaScript 4", body: "Описание поста" },
       ],
+      dialogVisible: false,
     };
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisible = false;
     },
     removePost(post) {
       this.posts = this.posts.filter((p) => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
     },
   },
 };
